47c101e6c4bfd697e6f0daf04cf26db986a5e049
caiopgaspar <c-gaspar@hotmail.com>
2026-06-24 09:59:41 -0300

    Add Book and User Repository

---
+++ backend/src/main/java/com/bookmanager/backend/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.bookmanager.backend.repository;
+
+import com.bookmanager.backend.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findAll();
+
+}
